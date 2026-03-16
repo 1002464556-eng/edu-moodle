7cd51cb925ea95c9d45f6b497a0db3eb5cfd34e1
1002464556-eng <1002464556@manhischools.org.il>
2026-03-16 16:38:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ import plotly.express as px
 # הגדרת תצורת העמוד
 st.set_page_config(page_title="דשבורד מפקחים - מדעים ומתמטיקה", layout="wide", page_icon="📊")
 
-# פונקציה לטעינת הנתונים
+# פונקציה לטעינת הנתונים (כולל משקפי קסם לעברית!)
 @st.cache_data
 def load_data():
-    df1 = pd.read_csv('מתמטיקה+מדעים _ מחוז ועיר 16.03.csv')
-    df2 = pd.read_csv('ללא קורסים.csv')
+    try:
+        df1 = pd.read_csv('מתמטיקה+מדעים _ מחוז ועיר 16.03.csv', encoding='utf-8-sig')
+        df2 = pd.read_csv('ללא קורסים.csv', encoding='utf-8-sig')
+    except:
+        df1 = pd.read_csv('מתמטיקה+מדעים _ מחוז ועיר 16.03.csv', encoding='cp1255')
+        df2 = pd.read_csv('ללא קורסים.csv', encoding='cp1255')
     return df1, df2
 
 df1, df2 = load_data()
